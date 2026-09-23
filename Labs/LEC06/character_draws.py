0952aa0a1c03f6be44bf72ecd94f94561f1c30a8
rbseo0814-tuk <rbseo0814@gmail.com>
2026-09-23 14:22:02 +0900

    move_three 정의

--- Labs/LEC06/character_draws.py
+++ Labs/LEC06/character_draws.py
@@ -60,7 +60,9 @@ def move_one():
 def move_two():
     move_line(600, 550 - 400 * math.sqrt(3) / 2, 200, 550 - 400 * math.sqrt(3) / 2)
 
+def move_three():
 
+    
 def move_triangle():
     move_one()
     move_two()
