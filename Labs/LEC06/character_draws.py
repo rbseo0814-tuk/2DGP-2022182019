78548753925edb7b5a9cafae3ed73da63c4c449b
rbseo0814-tuk <rbseo0814@gmail.com>
2026-09-23 14:19:31 +0900

    move_line의 y값 정의

--- Labs/LEC06/character_draws.py
+++ Labs/LEC06/character_draws.py
@@ -51,7 +51,7 @@ def move_line(x1, y1, x2, y2, steps=140):
     for i in range(steps + 1):
         t = i / steps
         x = x1 + (x2 - x1) * t
-
+        y = y1 + (y2 - y1) * t
 
 def move_triangle():
     move_one()
